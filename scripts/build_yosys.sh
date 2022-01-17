3fe30b048e0356d69b4de972600972e37351b889
Yichao Yu <yyc1992@gmail.com>
2022-01-16 19:12:23 -0500

    Tweak export functions to fit our need
    No need to export main functions now since we don't really need them and the main function
    uses NODEFS which we don't really need or want to include.
    Without the initialization in main, however, we have to manually call `Yosys::yosys_setup()`
    so include that in the export.

--- scripts/build_yosys.sh
+++ scripts/build_yosys.sh
@@ -8,6 +8,9 @@ export CXXFLAGS='-g0 -Os'
 export LDFLAGS='-g0 -Os -s INVOKE_RUN=0 -s MODULARIZE=1'
 export EM_NODE_JS="$builder_dir/scripts/node_wrapper"
 
+# _ZN5Yosys11yosys_setupEv is the mangled name for `Yosys::yosys_setup()`
+# We need to call this if we don't run main.
+
 cd "$source_dir"
 make config-emcc
-make yosys.js EMCCFLAGS='-g0 -Os -Wno-warn-absolute-paths -Wno-unused-command-line-argument --memory-init-file 0 --embed-file share -s NO_EXIT_RUNTIME=1 -s TOTAL_MEMORY=134217728 -s EXPORTED_FUNCTIONS=["_main","_run","_errmsg"] -s EXPORTED_RUNTIME_METHODS=["ccall","cwrap","callMain","FS"]'
+make yosys.js EMCCFLAGS='-g0 -Os -Wno-warn-absolute-paths -Wno-unused-command-line-argument --memory-init-file 0 --embed-file share -s NO_EXIT_RUNTIME=1 -s TOTAL_MEMORY=134217728 -s EXPORTED_FUNCTIONS=["_run","_errmsg","__ZN5Yosys11yosys_setupEv"] -s EXPORTED_RUNTIME_METHODS=["ccall","cwrap","FS"]'
