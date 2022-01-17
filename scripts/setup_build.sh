ef47f09bd86b7d8f31f4618adde1745aecd00106
Yichao Yu <yyc1992@gmail.com>
2022-01-16 22:32:05 -0500

    Add patches
    To get better error messages.

--- scripts/setup_build.sh
+++ scripts/setup_build.sh
@@ -4,3 +4,8 @@ builder_dir=$(realpath "$1")
 source_dir=$(realpath "$2")
 
 sudo pacman -S --noconfirm emscripten wasi-compiler-rt wasi-libc++ wasi-libc++abi
+
+cd "$source_dir"
+
+patch -Np1 < "$builder_dir/patches/0001-Do-not-load-host-file-system.patch"
+patch -Np1 < "$builder_dir/patches/0001-Include-prefix-in-the-logged-error.patch"
