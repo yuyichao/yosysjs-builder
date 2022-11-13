0547a466e4174018b4d772fe5274e8218584863d
Yichao Yu <yyc1992@gmail.com>
2022-11-13 08:46:13 -0500

    Try fixing build...

--- scripts/setup-archlinux.sh
+++ scripts/setup-archlinux.sh
@@ -7,6 +7,7 @@ pacman-key --init
 pacman-key --populate archlinux
 
 pacman -Syu --noconfirm devtools
+pacman -Rdd --noconfirm breezy # This ships a tools module that breaks the build.
 systemd-machine-id-setup # devtools requires a machine ID
 sed -i "s|MAKEFLAGS=.*|MAKEFLAGS=-j$(nproc)|" /etc/makepkg.conf
 useradd -m user # makepkg needs to be run with non-root
