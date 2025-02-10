467faed20ad4bc1933504a1677500ca1d69b46b5
Yichao Yu <yyc1992@gmail.com>
2025-02-10 11:22:31 -0500

    Fix github output deprecation

--- scripts/check_and_compute_versions.sh
+++ scripts/check_and_compute_versions.sh
@@ -20,17 +20,17 @@ read_yosysjs_version() {
 read_yosysjs_version
 
 if [ "$yosys_ver" = "$old_yosys_ver" ] && [ "$build_ver" = "$old_build_ver" ]; then
-    echo '::set-output name=uptodate::1'
+    echo "uptodate=1" >> $GITHUB_OUTPUT
     exit 0
 fi
 
-echo '::set-output name=uptodate::0'
+echo "uptodate=0" >> $GITHUB_OUTPUT
 if ! [[ "$yosys_ver" =~ ^([0-9]+)\.([0-9]+)$ ]]; then
     echo "Unexpected yosys version format: ${yosys_ver}" >&2
     exit 1
 fi
 
-echo "::set-output name=version::${BASH_REMATCH[1]}.${BASH_REMATCH[2]}.$build_ver"
+echo "version=${BASH_REMATCH[1]}.${BASH_REMATCH[2]}.$build_ver" >> $GITHUB_OUTPUT
 {
     echo "yosys_ver=$yosys_ver"
     echo "build_ver=$build_ver"
