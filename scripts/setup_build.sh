e369ba48fe11ae36615ef8fc832e3c299dd03432
Yichao Yu <yyc1992@gmail.com>
2025-02-10 11:47:17 -0500

    Install tcl as build depends

--- scripts/setup_build.sh
+++ scripts/setup_build.sh
@@ -3,7 +3,7 @@
 builder_dir=$(realpath "$1")
 source_dir=$(realpath "$2")
 
-sudo pacman -S --noconfirm emscripten wasi-compiler-rt wasi-libc++ wasi-libc++abi
+sudo pacman -S --noconfirm emscripten wasi-compiler-rt wasi-libc++ wasi-libc++abi tcl
 
 cd "$source_dir"
 
